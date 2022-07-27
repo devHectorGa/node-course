b7ee27a2c3d4cfe01708682db06cd5334752d9c7
Hector Garzon <hferney@technisys.com>
2022-07-27 12:24:59 -0500

    Inicio de proyecto - Lista de Tareas

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 require('colors');
-const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('./config/yargs');
-
 console.clear();
 
-crearArchivo(argv.b, argv.h, argv.l)
-  .then((nombreArchivo) => console.log(nombreArchivo.rainbow, 'creado'))
-  .catch((err) => console.error(err));
+const main = async () => {
+  console.log('Hola Mundo');
+};
+
+main();
