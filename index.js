ad38fc316215acd43ba55e035adbb0b8254bbaf6
Hector Garzon <hferney@technisys.com>
2022-07-28 10:08:30 -0500

    Modelo para controlar la aplicación

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import { inquirerMenu, pause, readInput } from './helpers/inquirer.js';
+import Searches from './models/searches.js';
 
 const main = async () => {
   let opt = '';
+  const searches = new Searches();
   do {
     opt = await inquirerMenu();
 
     switch (opt) {
       case 1:
-        const desc = await readInput('Ciudad a buscar:');
+        const place = await readInput('Ciudad:');
         break;
       case 2:
         console.log('Historial');
---
+++ models/searches.js
@@ -0,0 +1,10 @@
+export default class Searches {
+  constructor() {
+    this._history = [];
+  }
+
+  async city(place = '') {
+    console.log(place);
+    return [];
+  }
+}
