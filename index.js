b9a1a8775ba08b44d0bebd78ba7d877ab3da7f0a
Hector Garzon <hferney@technisys.com>
2022-07-28 10:02:50 -0500

    Menu de la aplicación

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import { readInput } from './helpers/inquirer';
+import { inquirerMenu, pause, readInput } from './helpers/inquirer.js';
 
 const main = async () => {
-  const text = await readInput();
+  let opt = '';
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        const desc = await readInput('Ciudad a buscar:');
+        break;
+      case 2:
+        console.log('Historial');
+        break;
+    }
+
+    opt !== 0 && (await pause());
+  } while (opt !== 0);
 };
 
 main();
