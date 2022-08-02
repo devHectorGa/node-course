377fd8e6c464f68697199785c489919789371e9b
Hector Garzon <hferney@technisys.com>
2022-08-02 14:54:49 -0500

    100. Iniciando el proyecto - RESTServer

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import hbs from 'hbs';
 import { config } from 'dotenv';
 
 config();
@@ -11,33 +10,6 @@ const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const params = {
-  title: 'Node Course',
-  name: 'devHectorGa',
-};
-
-hbs.registerPartials(__dirname + '/views/partials');
-
-app.set('view engine', 'hbs');
-
-app.use(express.static('public'));
-
-app.get('/', (_, res) => {
-  res.render('home', params);
-});
-
-app.get('/generic', (_, res) => {
-  res.render('generic', { ...params, headerClass: 'alt' });
-});
-
-app.get('/elements', (_, res) => {
-  res.render('elements', { ...params, headerClass: 'alt' });
-});
-
-app.get('*', (_, res) => {
-  res.sendFile(__dirname + '/public/404.html');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
