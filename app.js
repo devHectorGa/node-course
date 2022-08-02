0956d96b1d2f9ca2a6126048f7bfb73e57c779c3
Hector Garzon <hferney@technisys.com>
2022-08-02 08:59:01 -0500

    91. Handlebars

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const port = 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo');
+app.get('/', (_, res) => {
+  res.render('home');
 });
 
 app.get('/generic', (_, res) => {
