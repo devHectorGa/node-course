63fa737eb91debbe5677056fdef4ab23fdb4c896
Hector Garzon <hferney@technisys.com>
2022-08-02 08:51:49 -0500

    90. Servir un sitio web completo

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 const app = express();
 const port = 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
+});
+
+app.get('/generic', (_, res) => {
+  res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (_, res) => {
+  res.sendFile(__dirname + '/public/elements.html');
+});
+
+app.get('*', (_, res) => {
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
