795258fd1e85bf19f3de92a72a6b047718d484b5
Hector Garzon <hferney@technisys.com>
2022-08-08 18:05:29 -0500

    141. Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,19 @@
-export const login = (_, res) => {
+import { UserModel } from '../models/user.js';
+import bcryptjs from 'bcryptjs';
+
+const INVALID_CREDENTIAL_MESSAGE = 'Usuario / Correo no son correctos';
+
+export const login = async ({ body: { email, password } }, res) => {
+  try {
+    const user = await UserModel.findOne({ email, state: true });
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!user || !validPassword) {
+      return res.status(400).json({ message: INVALID_CREDENTIAL_MESSAGE });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `Error, algo salio mal` });
+  }
+
   res.json({ message: 'Login' });
 };
