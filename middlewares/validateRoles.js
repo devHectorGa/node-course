07300061c9c3e371cc23cdd04506953f6e9ef525
Hector Garzon <hferney@technisys.com>
2022-08-09 14:05:39 -0500

    147. Middleware: tiene rol

--- middlewares/validateRoles.js
+++ middlewares/validateRoles.js
@@ -1,4 +1,4 @@
-export const isAdminRole = ({ user }, res, next) => {
+const validUser = (user, res) => {
   if (!user) {
     console.error(
       `Use "isAdminRole" before validate token and include user in request`
@@ -7,5 +7,22 @@ export const isAdminRole = ({ user }, res, next) => {
       .status(500)
       .json({ message: 'Error please contact with maintainer' });
   }
+};
+
+export const isAdminRole = ({ user }, res, next) => {
+  validUser(user, res);
+  if (user.rol !== 'ADMIN') {
+    return res.status(401).json({ message: 'User is not administrator' });
+  }
   next();
+};
+
+export const haveRol = (...roles) => {
+  return ({ user }, res, next) => {
+    validUser(user, res);
+    if (!roles.includes(user.role)) {
+      return res.status(401).json({ message: `Usuario no autorizado` });
+    }
+    next();
+  };
 };
